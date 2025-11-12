f8fb4a6898dc91ebe4f5894efa88acf4e22acbc5
SHAIK ABDUL HAFEEZ <168513325+hafeezabdulshaik08@users.noreply.github.com>
2025-11-13 00:04:38 +0530

    Update hafeez.js

--- hafeez.js
+++ hafeez.js
@@ -1,3 +1,94 @@
+2a//
+const express = require(express');
+const app = express0;
+const PORT = 3000;
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+// Routes
+app.get(/', (req, res) => {
+const data = { title: 'Home Page', message: 'Welcome to EJS Example!' };
+res.render('sample', data); // Render the 'index.ejs' template
+3):
+// Start server
+app. listen PORT, 0 => {
+console.log( Server running at http://localhost:${PORT;*);
+3);
+server.js
+L views/
+L index.ejs
+Create views/index.ejs
+‹!DOCTYPE html >
+<html>
+<head>
+<title><%= title %></title>
+</head> <body>
+<h1><%= message %></h1>
+<p> This page is rendered using the <b> EJS</b> templating engine.</p>
+</body> </html>
+ 
+//6a
+import React from "react"; 
+import Welcome from "./Welcome"; 
+function App() { 
+return ( 
+<div style={{ fontFamily: "system-ui", padding: 20 }}> 
+<h2>React Props and State Example</h2> 
+{/* Pass data (props) to child component */} 
+<Welcome name="Alice" /> 
+<Welcome name="Bob" /> 
+</div> 
+); 
+} 
+export default App 
+Welcome.js 
+import React, { useState } from "react"; 
+function Welcome({ name }) { 
+// State to store number of greetings 
+const [count, setCount] = useState(0); 
+return ( 
+<div 
+style={{ 
+border: "1px solid #ccc", 
+padding: "10px", 
+margin: "10px 0", 
+borderRadius: "8px", 
+}} 
+> 
+<h3>Hello, {name}  </h3> 
+<p>You have greeted {count} times.</p> 
+<button onClick={() => setCount(count + 1)}>Say Hello</button> 
+</div> 
+); 
+} 
+export default Welcome; 
+index.js 
+import React, { StrictMode } from "react"; 
+import { createRoot } from "react-dom/client"; 
+import React, { StrictMode } from "react";
+ import { createRoot } from "react
+ import "./styles.css"; 
+import App from "./App"; 
+ateRoot(document.getElementById("root")); 
+const root = createRoot(document.getElementById("root"));
+ root.render( 
+<StrictMode> 
+<App /> 
+</StrictMode> 
+); 
+index.html 
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device
+ <title>Document</title> 
+</head> 
+<body> 
+<div id="root"></div> 
+</body> 
+</html>
+
+//12
 import React, { useState } from "react"; 
  
 function App() { 
